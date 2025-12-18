5f83d9e75a1cb3030c3f9192dfe2c558b94f002f
Tapan Bhakhar <tapanbhakhar021@gmail.com>
2025-12-18 14:30:15 +0530

    feat: by using express server running status done

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const env = require("dotenv");
+env.config();
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+
+const startServer = async () => {
+  try {
+    // await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
